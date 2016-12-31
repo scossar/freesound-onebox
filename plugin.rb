0340590d702960749970b9c0436359c532743869
scossar <scossar3@gmail.com>
2016-12-30 22:11:23 -0800

    Remove comment

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,6 @@ class Onebox::Engine::FreesoundOnebox
   include Onebox::Engine
   include StandardEmbed
 
-  # matches_regexp /^https?:\/\/www\.freesound\.org\/people(\/?([a-zA-Z0-9]-_))+\/(\d+)\/?(\?.*)?$/
   matches_regexp /^https?:\/\/www\.freesound\.org\/people\/.+\/(\d+)\/?(\?.*)?$/
 
   def wave_image_url
